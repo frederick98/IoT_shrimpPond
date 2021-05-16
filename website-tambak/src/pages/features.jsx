89f6b72b3dacc75e1a31e6a30791fafe3a87abad
frederick <fritzfrederick51@gmail.com>
2021-05-16 20:28:31 +0700

    page reworked
    just finished with designing location, hardwares, and features page. its designs are now follows previous mockups.

--- website-tambak/src/pages/features.jsx
+++ website-tambak/src/pages/features.jsx
@@ -3,66 +3,25 @@ import { Link } from "react-router-dom";
 
 const Features = () => {
   return (
-    <div>
-      <div>
-        <h1>Application's Features</h1>
+    <div class="container">
+      <div class="row">
+        <div class="my-4 col-auto">
+          <h1>Application Features</h1>
+        </div>
       </div>
-      <div class="row align-items-center my-6">
-        <div class="col-lg-5">
+      <div class="row">
+        <div class="col-7">
           <img
             class="img-fluid rounded mb-4 mb-lg-0"
             src="https://via.placeholder.com/600x400"
             alt="..."
           />
         </div>
-        <div class="col-lg-4">
-          <h3 class="font-weight-light">Monitoring</h3>
-          <p>
-            This application are able to do a water quality monitoring using a
-            wireless sensor network. It gives user a real-time monitoring to
-            control the quality of the water.
-          </p>
-          {/* <a class="btn btn-primary" href="#">
-            Action!
-          </a> */}
-        </div>
-      </div>
-      <div class="my-5"></div>
-      <div class="row align-items-center my-6">
-        <div class="col-lg-5">
-          <img
-            class="img-fluid rounded mb-4 mb-lg-0"
-            src="https://via.placeholder.com/600x400"
-            alt="..."
-          />
-        </div>
-        <div class="col-lg-4">
-          <h3 class="font-weight-light">Node Checking</h3>
-          <p>
-            What if some of the sensor's node are not working? What if static
-            data shown? How do you check whether all of the node is online? Here
-            we have a feature that allows user to check all the node status, and
-            reports to user if something is not right.
-          </p>
-          {/* <a class="btn btn-primary" href="#">
-            Action!
-          </a> */}
-        </div>
-      </div>
-      <div class="my-5"></div>
-      <div class="row align-items-center my-6">
-        <div class="col-lg-5">
-          <img
-            class="img-fluid rounded mb-4 mb-lg-0"
-            src="https://via.placeholder.com/600x400"
-            alt="..."
-          />
-        </div>
-        <div class="col-lg-4">
+        <div class="col-5">
           <h3 class="font-weight-light">
             Data Graphics Diagram (<strong>BETA</strong>)
           </h3>
-          <p>
+          <p class="text-justify">
             Result received from Data's Monitoring will be shown in a line
             diagram chart. A line chart will be shown to user, which received
             real-time data directly from the sensor's reading.{" "}
@@ -71,33 +30,90 @@ const Features = () => {
               not showing accurate data.
             </strong>
           </p>
-          {/* <a class="btn btn-primary" href="#">
-            Action!
-          </a> */}
         </div>
       </div>
-      <div class="my-5"></div>
-      <div class="row align-items-center my-6">
-        <div class="col-lg-5">
+      <div class="my-3"></div>
+      <div class="row">
+        <div class="col-7">
           <img
             class="img-fluid rounded mb-4 mb-lg-0"
             src="https://via.placeholder.com/600x400"
             alt="..."
           />
         </div>
-        <div class="col-lg-4">
-          <h3 class="font-weight-light">Easy-Login and Registration</h3>
-          <p>
-            Log into the application easily using your username and password.
-            User can access it everywhere as long as you write the correct
-            address. If you don't have an ID, feel free to contact us to make
-            you a new ID and password.
+        <div class="col-5">
+          <h3 class="font-weight-light">Monitoring</h3>
+          <p class="text-justify">
+            This application are designed to do a water quality monitoring using
+            a wireless sensor network. It gives user a real-time monitoring to
+            control the quality of the water.
           </p>
-          {/* <a class="btn btn-primary" href="#">
-            Action!
-          </a> */}
         </div>
       </div>
+      <div class="my-3"></div>
+      <div class="row">
+        <div class="col-7">
+          <img
+            class="img-fluid rounded mb-4 mb-lg-0"
+            src="https://via.placeholder.com/600x400"
+            alt="..."
+          />
+        </div>
+        <div class="col-5">
+          <h3 class="font-weight-light">Node Checking</h3>
+          <p class="text-justify">
+            What if some of the sensor's node are not working? What if static
+            data shown? How do you check whether all of the node is online? Here
+            we have a feature that allows user to check all the node status, and
+            reports to user if something is not right.
+          </p>
+        </div>
+      </div>
+      <div class="my-3"></div>
+      <div class="row">
+        <div class="col-7">
+          <img
+            class="img-fluid rounded mb-4 mb-lg-0"
+            src="https://via.placeholder.com/600x400"
+            alt="..."
+          />
+        </div>
+        <div class="col-5">
+          <h3 class="font-weight-light">Easy-Login and Registrations</h3>
+          <p class="text-justify">
+            Log into the application easily using your username and password.
+            User can access it everywhere as long as you write the correct
+            address.
+          </p>
+          <p class="text-justify">
+            If you don't have an ID, feel free to contact us to make you a new
+            ID and password.
+          </p>
+        </div>
+      </div>
+      <div class="my-3"></div>
+      <div class="row">
+        <div class="col-7">
+          <img
+            class="img-fluid rounded mb-4 mb-lg-0"
+            src="https://via.placeholder.com/600x400"
+            alt="..."
+          />
+        </div>
+        <div class="col-5">
+          <h3 class="font-weight-light">Bug Reporting</h3>
+          <p class="text-justify">
+            If you ever found any bugs, or you wanted to submit a feature
+            suggestions, we're pleased to know that!
+          </p>
+          <p class="text-justify">
+            Reach us by going to the "About Us" section, and contact us uding
+            any of the available number. Any suggestion and critics will help us
+            design this application to be better :D
+          </p>
+        </div>
+      </div>
+      <div class="my-3"></div>
     </div>
   );
 };
